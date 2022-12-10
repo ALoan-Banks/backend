a92894502c54e340887217da92bdaefc2fd8430d
SergioHPassos <shp360@hotmail.com>
2022-12-10 10:19:33 -0500

    add resetPassword method to UserService class

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import com.revature.dtos.ResetPasswordRequest;
+import com.revature.exceptions.EmailDoesntExistException;
 import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -29,5 +31,22 @@ public class UserService {
 
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    public User resetPassword(ResetPasswordRequest resetPasswordRequest) {
+        // get user from database
+        User updateUser = userRepository.findByEmail(resetPasswordRequest.getEmail()).isPresent()?userRepository.findByEmail(resetPasswordRequest.getEmail()).get():null;
+
+        if(updateUser==null){
+            throw new EmailDoesntExistException("Email doesn't exist");
+        }
+
+        // check DOB matches with user
+        if(updateUser.getDOB().equalsIgnoreCase(resetPasswordRequest.getDOB())){
+            updateUser.setPassword(resetPasswordRequest.getNewPassword());
+        }
+
+        // return user
+        return userRepository.save(updateUser);
     }
 }
