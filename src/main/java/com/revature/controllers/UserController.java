1681616418f58dc93af6cd8fed77715db78db74c
Sergio H. Passos <48944090+SergioHPassos@users.noreply.github.com>
2022-12-22 04:47:31 -0500

    update jar file

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -13,12 +13,13 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://aloanbanks-frontend-bucket.s3-website-us-east-1.amazonaws.com", "*"} )
+//@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://aloanbanks-frontend-bucket.s3-website-us-east-1.amazonaws.com"}, allowCredentials = "true")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+//    @Authorized
 //    @Authorized
     @Transactional
     @GetMapping("/{id}")
